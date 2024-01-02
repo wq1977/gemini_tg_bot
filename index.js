5e7523e339eeddbf4d6c316b53d2d160d22bfed7
wesley <9610848@qq.com>
2024-01-02 23:01:20 +0800

    change to oracle

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ if (!token) {
   throw new Error("需要先设置 Telegram 的 bot token");
 }
 let options = {
-  polling: true,
+  polling: require("config").get("telegram.host") ? false : true,
 };
 if (process.env.https_proxy) {
   options = {
@@ -19,13 +19,34 @@ const bot = new TelegramBot(token, options);
 const log = require("./log")("main");
 gemini.bot = bot;
 
-setInterval(() => {
-  const polling = bot.isPolling();
-  if (!polling) {
-    bot.startPolling({ restart: true });
-  }
-  log.info({ polling }, "check bot running ...");
-}, 10000);
+if (require("config").get("telegram.host")) {
+  log.info("set to webhook mode");
+  const express = require("express");
+  const app = express();
+  app.use(express.json());
+  app.post(`${require("config").get("telegram.url")}${token}`, (req, res) => {
+    bot.processUpdate(req.body);
+    res.sendStatus(200);
+  });
+  const port = require("config").get("telegram.port");
+  app.listen(port, () => {
+    log.info(`Express server is listening on ${port}`);
+  });
+  bot.setWebHook(
+    `https://${require("config").get("telegram.host")}${require("config").get(
+      "telegram.url"
+    )}${token}`
+  );
+} else {
+  log.info("set to polling mode");
+  setInterval(() => {
+    const polling = bot.isPolling();
+    if (!polling) {
+      bot.startPolling({ restart: true });
+    }
+    log.info({ polling }, "check bot running ...");
+  }, 10000);
+}
 
 const runCommand = require("./commands");
 runCommand.init(bot);
